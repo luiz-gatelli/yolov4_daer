e260a2a753d15452f4a928fb86fa2d421c997bcc
Luiz Gatelli <luiz.c.gatelli@gmail.com>
2021-02-24 22:25:32 -0300

    added small sh file to move files from some directory

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
-cd /gdrive/My Drive/unprocessed_videos/
+cd "/gdrive/My Drive/"
+echo "Copying Weigths:"
+cp "yolov4-obj_best.weights" -P "/content/yolov4_daer/data/yolov4.weights"
+echo "Copied YoloV4 Weigths"
+
+echo ""
+
+echo "Copying Videos:"
+
+cd "/gdrive/My Drive/unprocessed_videos/"
 for file in *
 do 
-    echo "$file"
+    cp $file -P "/content/yolov4_daer/data/video/$file"
+    echo "Copied file: $file"
 done
 
+echo ""
+echo "Saving Model:"
 
+cd /content/yolov4_daer/
+python save_model.py --model yolov4
+
+echo "Model Saved"
