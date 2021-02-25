ec1992b4c0233289105e9f3a21c3d5df629a2c6a
Luiz Gatelli <luiz.c.gatelli@gmail.com>
2021-02-24 22:37:06 -0300

    added copy data sh script

---
+++ move_generated_data.sh
@@ -0,0 +1,14 @@
+cd "/tracks/"
+
+for file in *
+do
+    cp $file  -P "/../gdrive/My Drive/daer_output_data/$file"
+    echo "Copied file: $file"
+done
+
+cd "/outputs/"
+for file in *
+do
+    cp $file  -P "/../gdrive/My Drive/daer_output_data/$file"
+    echo "Copied file: $file"
+done
